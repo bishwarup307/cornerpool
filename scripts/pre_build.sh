d6abcecc9c42fe2b2745148d5490c97b6849a367
bishwarup307 <write2bishwarup@gmail.com>
2021-01-04 15:25:49 +0530

    modify worlflow

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -1,18 +1,7 @@
 yum install wget
 
-mkdir ~/glibc_install; cd ~/glibc_install 
-wget http://ftp.gnu.org/gnu/glibc/glibc-2.14.tar.gz
-tar zxvf glibc-2.14.tar.gz
-cd glibc-2.14
+pip install torch==1.7.1+cpu -f https://download.pytorch.org/whl/torch_stable.
+wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.7.1%2Bcpu.zip
+unzip libtorch-shared-with-deps-1.7.1+cpu -d /opt
 
-mkdir build
-cd build
-
-../configure --prefix=/opt/glibc-2.14
-
-make -j4
-
-make install
-
-export LD_LIBRARY_PATH="/opt/glibc-2.14/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-pip install torch
+export LD_LIBRARY_PATH="/opt/libtorch/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"

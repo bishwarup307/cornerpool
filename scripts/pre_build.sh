76f3dbeb76527dd8f3243cf195d68c4f24912c57
bishwarup307 <write2bishwarup@gmail.com>
2021-01-04 16:00:14 +0530

    modify worlflow

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -2,11 +2,9 @@ yum install wget
 echo "installed wget"
 
 pip install torch==1.7.1+cpu -f https://download.pytorch.org/whl/torch_stable
-
 echo "installed pytorch"
 
 wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.7.1%2Bcpu.zip
-
 echo "downloaded pytorch c++ libraries"
 
 unzip libtorch-shared-with-deps-1.7.1+cpu -d /opt

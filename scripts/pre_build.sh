7222d5410377348a213b596ff21b1281da31a360
bishwarup307 <write2bishwarup@gmail.com>
2021-01-04 19:13:34 +0530

    modify worlflow

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -4,9 +4,9 @@ echo "installed wget"
 # python3 -m 
 # echo "installed pytorch"
 
-wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.7.1%2Bcpu.zip
+wget -q https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.7.1%2Bcpu.zip
 echo "downloaded pytorch c++ libraries"
 
-unzip libtorch-shared-with-deps-1.7.1+cpu -d /opt
+unzip -qq libtorch-shared-with-deps-1.7.1+cpu -d /opt
 
 # export LD_LIBRARY_PATH="/opt/libtorch/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"

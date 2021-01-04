0f12ef700a636c74aa26a34f276f31c3e43e3db8
bishwarup307 <write2bishwarup@gmail.com>
2021-01-04 20:32:22 +0530

    fix workflow

--- scripts/pre_build_macos.sh
+++ scripts/pre_build_macos.sh
@@ -1,3 +1,3 @@
 wget -q wget https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.7.1.zip
-unzip -qq libtorch-macos-1.7.1.zip -d /opt
+unzip -qq libtorch-macos-1.7.1.zip -d /libs
 rm -f libtorch-macos-1.7.1.zip
